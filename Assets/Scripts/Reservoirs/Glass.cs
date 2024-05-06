3f59cbf16f82ca6b0676d4d6a5e143db85047e74
SergeiKonkov <sergeirest@mail.ru>
2024-05-06 13:25:16 +0300

    update glass drag

--- Assets/Scripts/Reservoirs/Glass.cs
+++ Assets/Scripts/Reservoirs/Glass.cs
@@ -1,14 +1,16 @@
 ﻿using Scripts.Reservoirs.ReservoirMover;
+using UnityEngine;
 using Zenject;
 
 namespace Scripts.Reservoirs
 {
     public class Glass : Reservoir
     {
+        [SerializeField] private Transform _spoon;
         [Inject]
         protected void Construct(DiContainer diContainer)
         {
-            mover = new GlassMover(transform);
+            mover = new GlassMover(_spoon);
             diContainer.Inject(mover);
             base.Construct();
         }
--- Assets/Scripts/Reservoirs/ReservoirMover/GlassMover.cs
+++ Assets/Scripts/Reservoirs/ReservoirMover/GlassMover.cs
@@ -6,6 +6,12 @@ namespace Scripts.Reservoirs.ReservoirMover
     public class GlassMover : IMover
     {
         private Transform _movableObject;
+        private Vector3 _defaultPositon;
+        private float currentTime = 0;
+        private float maxTime = 2;
+
+        private float minClamp = -1;
+        private float maxClamp = 1;
 
         public event Action OnComplete;
 
@@ -13,16 +19,28 @@ namespace Scripts.Reservoirs.ReservoirMover
         public GlassMover(Transform movableObject)
         {
             _movableObject = movableObject;
+            _defaultPositon = movableObject.transform.localPosition;
+            Debug.Log(_defaultPositon);
         }
 
         public void Move(Vector3 movePosition)
         {
+            Vector3 toMove = new Vector3(movePosition.x, _movableObject.position.y);
+            toMove.x = Mathf.Clamp(toMove.x, minClamp, maxClamp);
             
+            _movableObject.transform.position = toMove;
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= maxTime)
+            {
+                currentTime = 0;
+                OnComplete?.Invoke();
+            }
         }
 
         public void SetToDefault()
         {
-            
+            _movableObject.transform.localPosition = _defaultPositon;
         }
     }
 }
